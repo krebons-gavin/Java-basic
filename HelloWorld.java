a1c3214f3bc72dd8e6fc2b02b6a7ae671a2fb0a6
krebons-gavin <krebons@me.com>
2015-12-25 16:54:28 +0800

    2015-12-25, Added by Gavin

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Gavin on 2015-07-28
+ * Modified at 2015-9-5.
+ * My Java Hello World
+ */
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello Gavin, Created at 2015-07-28");
+        System.out.println("Hello 世界, Modified at 2015-09-05");
+
+        System.out.println("基本类型：byte 二进制位数：" + Byte.SIZE);
+    }
+}
